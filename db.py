d7e2850b4a7cad20d1858b138d0c676edd79a50d
hollyw00d337 <121274226+hollyw00d337@users.noreply.github.com>
2025-06-15 22:16:21 -0600

    Update db.py

--- db.py
+++ db.py
@@ -1,21 +1,21 @@
 from pymongo import MongoClient
 
 # URL de conexión a tu instancia de MongoDB
-MONGO_URI = "mongodb://localhost:27017/nombre_de_tu_base_de_datos"  # Cambia esto por tu URI de MongoDB
+MONGO_URI = "mongodb://localhost:27017/nombre_de_tu_base_de_datos"  
 
-# Inicializar la conexión
+# Inicializar la conexion
 client = MongoClient(MONGO_URI)
 
 # Seleccionar la base de datos
-db = client["nombre_de_tu_base_de_datos"]  # Cambia "nombre_de_tu_base_de_datos" por el nombre de tu base de datos
+db = client["nombre_de_tu_base_de_datos"]  
 
 def get_menu_items():
     """Función para obtener todos los elementos del menú."""
-    menu_collection = db["menu"]  # Cambia "menu" por el nombre de tu colección
+    menu_collection = db["menu"]  
     return list(menu_collection.find())
 
 def add_menu_item(data):
     """Función para agregar un nuevo elemento al menú."""
-    menu_collection = db["menu"]  # Cambia "menu" por el nombre de tu colección
+    menu_collection = db["menu"]  
     result = menu_collection.insert_one(data)
     return result.inserted_id
